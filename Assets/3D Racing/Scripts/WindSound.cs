cb2d4fbcf2fb418d0e745d3e025d54ad1d8b5f1b
gamesIQ007 <bdagames@ya.ru>
2023-02-19 16:04:32 +0300

    ЗАДАНИЕ 30.6.1
    В качестве итогового задания вам необходимо добавить элементы эстетики.
    
    Есть окружение на трассе (в виде примитивных блоков или моделей.
    
    Дополнительно:
    Музыка на фоне;
    Размытие и виньетка — вот вот этого пока нет;
    Звук переключения передач и ветра - без переключения передач.

---
+++ Assets/3D Racing/Scripts/WindSound.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Racing
+{
+    [RequireComponent(typeof(AudioSource))]
+
+    /// <summary>
+    /// Звук ветра при движении
+    /// </summary>
+    public class WindSound : MonoBehaviour
+    {
+        /// <summary>
+        /// Автомобиль
+        /// </summary>
+        [SerializeField] private Car car;
+
+        /// <summary>
+        /// Минимальная нормализованная скорость для появления звука
+        /// </summary>
+        [SerializeField] [Range(0.0f, 1.0f)] private float minNormalizedSpeedForSound;
+
+        /// <summary>
+        /// Источник звука
+        /// </summary>
+        private AudioSource windAudioSource;
+
+        private void Start()
+        {
+            windAudioSource = GetComponent<AudioSource>();
+        }
+
+        private void Update()
+        {
+            if (car.NormalizedLinearVelocity >= minNormalizedSpeedForSound)
+            {
+                //windAudioSource.volume = car.NormalizedLinearVelocity; // (1 - minNormalizedSpeedForSound);
+                windAudioSource.volume = (car.NormalizedLinearVelocity - minNormalizedSpeedForSound) / (1 - minNormalizedSpeedForSound);
+            }
+            else
+            {
+                windAudioSource.volume = 0;
+            }
+        }
+    }
+}
